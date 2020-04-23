6564d5aa4faa302a092d2ff1aaf6a9273fe20dcc
qzagarese <quirino.zagarese@gmail.com>
2020-04-23 12:51:34 +0100

    Adding startup option to select network interface

--- streamesh-server/src/main/java/io/scicast/streamesh/server/StreaMeshServer.java
+++ streamesh-server/src/main/java/io/scicast/streamesh/server/StreaMeshServer.java
@@ -7,15 +7,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.NetworkInterface;
+import java.util.Collections;
 
 @SpringBootApplication
 public class StreaMeshServer {
 
+    private static final String STREAMESH_SERVER_NETWORK_INTERFACE = "streamesh.server.network.interface";
     private static String serverAddress;
 
     public static void main(String[] args) throws IOException {
         serverAddress = System.getProperty(StartupUtils.STREAMESH_SERVER_ADDRESS_PROPERTY);
+        if (serverAddress == null) {
+            serverAddress = Collections.list(NetworkInterface.getNetworkInterfaces()).stream()
+                    .filter(ni -> ni.getDisplayName().equals(System.getProperty(STREAMESH_SERVER_NETWORK_INTERFACE)))
+                    .map(ni -> Collections.list(ni.getInetAddresses()).stream()
+                            .filter(inetAddress -> inetAddress instanceof Inet4Address)
+                            .map(inetAddress -> inetAddress.getHostAddress())
+                            .findFirst()
+                            .orElse(null))
+                    .findFirst()
+                    .orElse(null);
+        }
         if (serverAddress == null) {
             serverAddress = StartupUtils.selectAddress();
         }
